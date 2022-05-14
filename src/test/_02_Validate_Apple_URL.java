4a430adb5c7c17712a44dbc40144d69b72b2a704
ReginaBik <reginabiktimirova1@gmail.com>
2022-05-14 15:30:10 -0500

    added URL test

---
+++ src/test/_02_Validate_Apple_URL.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class _02_Validate_Apple_URL {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.apple.com");
+        String expectedString = "https://www.apple.com/";
+        String actualString = driver.getCurrentUrl();
+        if(actualString.equals(expectedString)) System.out.println("Apple URL validation PASSED");
+        else System.out.println("Apple URL validation FAILED");
+
+        System.out.println("End of the program");
+
+        driver.quit();
+
+
+
+
+    }
+}
