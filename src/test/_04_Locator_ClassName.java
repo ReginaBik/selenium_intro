97c1152ada8dead91e47146318ffd77e1fccbe5c
ReginaBik <reginabiktimirova1@gmail.com>
2022-05-15 16:12:37 -0500

    added more automation tests

--- src/test/_04_Locator_ClassName.java
+++ src/test/_04_Locator_ClassName.java
@@ -30,5 +30,7 @@ public class _04_Locator_ClassName {
             // 3. Teardown
             Driver.quitDriver();
 
+
+
     }
 }
---
+++ src/test/_07_Text_Attribute_CssValues_of_Web_Elements.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.WebDriver;
+import utilities.Driver;
+
+public class _07_Text_Attribute_CssValues_of_Web_Elements {
+    public static void main(String[] args) {
+        WebDriver driver = Driver.getDriver();
+
+        driver.get("https://www.google.com/");
+
+
+    }
+}
